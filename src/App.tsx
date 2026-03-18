a1148399e5753e507e7d651f604a9297c0269e27
kris10ansn <kristian.nessa@protonmail.com>
2026-03-18 23:41:58 +0700

    feat: migrate to react-router-dom v7, replace SetLocation component with simple Navigate logic in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
 import React from "react";
-import {
-    Route,
-    BrowserRouter as Router,
-    Switch,
-    useHistory,
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MainMenu from "./MainMenu";
 import Minesweeper from "./Minesweeper";
 import Settings from "./Settings";
 import { getStoredState } from "./util/storedState";
 
-// Own component to be able to use the useHistory hook
-const SetLocation = () => {
-    const history = useHistory();
+const App: React.FC = () => {
     const gameState = getStoredState("state", null);
 
-    if (gameState !== null) {
-        history.push("/game");
-    } else {
-        history.push("/");
-    }
-
-    return <></>;
-};
-
-const App: React.FC = () => {
     return (
-        <Router>
-            <SetLocation />
-            <Switch>
-                <Route path="/game">
-                    <Minesweeper />
-                </Route>
-                <Route path="/settings">
-                    <Settings />
-                </Route>
-                <Route exact path="/">
-                    <MainMenu />
-                </Route>
-            </Switch>
-        </Router>
+        <BrowserRouter>
+            {/* Resume playing if there is a game in progress */}
+            {gameState !== null && <Navigate to="/game" />}
+
+            <Routes>
+                <Route path="/game" element={<Minesweeper />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/" element={<MainMenu />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
